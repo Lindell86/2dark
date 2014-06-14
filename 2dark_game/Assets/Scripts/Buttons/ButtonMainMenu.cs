b8acf8a64d5e670316a26716cd0aa635bb79042e
Lindell86 <sked86@hotmail.com>
2014-06-15 00:31:57 +0200

    added main menu with buttons and a game state

---
+++ 2dark_game/Assets/Scripts/Buttons/ButtonMainMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MainMenuButtonType
+{
+    START_GAME,
+    EXIT_GAME,
+};
+
+public class ButtonMainMenu : MonoBehaviour {
+
+    public MainMenuButtonType eButtonType = MainMenuButtonType.START_GAME;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseEnter()
+    {
+        Color newColor = Color.white;
+        newColor.r = 1.0f;
+        newColor.g = 1.0f;
+        newColor.b = 0.5f;
+        renderer.material.color = newColor;
+    }
+    void OnMouseExit()
+    {
+        Color newColor = Color.white;
+        renderer.material.color = newColor;
+    }
+
+    void OnMouseDown()
+    {
+        if (eButtonType == MainMenuButtonType.START_GAME)
+        {
+            Application.LoadLevel("GameLevel");
+        }
+        else if (eButtonType == MainMenuButtonType.EXIT_GAME)
+        {
+            Application.Quit();
+        }
+    }
+
+    internal MainMenuButtonType START_GAME { get; set; }
+}
